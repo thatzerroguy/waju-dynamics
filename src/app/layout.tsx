f4d69ddf601d59df2162663ebd7c916840e268a4
Nduka Ugochukwu <62814725+thatzerroguy@users.noreply.github.com>
2026-03-04 12:57:08 +0100

    fix: rocket message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body>{children}
 
-        <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Fhaulpro5016back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.17" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+      </body>
     </html>
   );
 }
